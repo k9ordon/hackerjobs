bf22960c4943d32f6261841e5813ead4bd5934d4
k9ordon <k9ordon@gmail.com>
2013-07-05 14:21:46 +0200

    better url handling

--- index.php
+++ index.php
@@ -9,14 +9,15 @@ include 'c/c.php';
 
 session_start();
 
-$p = parse_url($_SERVER['REQUEST_URI']);
-$up = split('/', $p['path']);
-$cn = array_key_exists(2, $up) && !empty($up[2]) ? $up[2] : 'index';
-$an = array_key_exists(3, $up) && !empty($up[3]) ? $up[3] : 'index';
+$p = split('\?', substr(sprintf('http://%s%s', $_SERVER['SERVER_NAME'], $_SERVER['REQUEST_URI']),strlen($config['base'])));
+$up = split('/', $p[0]);
+
+$cn = array_key_exists(1, $up) && !empty($up[1]) ? $up[1] : 'index';
+$an = array_key_exists(2, $up) && !empty($up[2]) ? $up[2] : 'index';
 
 $isAjax = array_key_exists('ajax', $_GET) ? true : false;
 
-//var_dump(array('cn' => $cn,'an' => $an));exit;
+//var_dump(array($p, $up, 'cn' => $cn,'an' => $an));exit;
 
 // run controller
 include sprintf('c/%s.php', $cn);

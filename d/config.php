8800b0c0b19251fdaed7f906b49abf05042bd368
k9ordon <k9ordon@gmail.com>
2013-07-05 14:29:08 +0200

    trim api key

--- d/config.php
+++ d/config.php
@@ -2,7 +2,7 @@
 $config['base'] = "http://hackerjobs.k94n.com";
 
 // get your api key at http://www.karriere.at/api/docs
-$config['apiKey'] = file_get_contents('d/apikey');
+$config['apiKey'] = trim(file_get_contents('d/apikey'));
 $config['apiUrl'] = 'http://www.karriere.at/api/%s?key='.$config['apiKey'].'&type=php&%s';
 $config['defaultLocation'] = 'Österreich';
 
